7f54179e1370c4d4f48350ab4d4274391539a12d
Terry Brady <terrywbrady@gmail.com>
2020-06-30 17:52:58 -0700

    Update uc3-mysql.sh

--- uc3-mysql.sh
+++ uc3-mysql.sh
@@ -8,5 +8,8 @@ check_ssm_root
 dbname=`get_ssm_value_by_name ${SSM_DB_NAME}/db-name`
 dbhost=`get_ssm_value_by_name ${SSM_DB_NAME}/db-host`
 dbuser=`get_ssm_value_by_name "${SSM_DB_NAME}/${SSM_DB_ROLE:-readonly}/db-user"`
+dbpass=`get_ssm_value_by_name "${SSM_DB_NAME}/${SSM_DB_ROLE:-readonly}/db-password"`
 
-echo "H:${dbhost} D:${dbname} U:${dbuser} "
+export MYSQL_PWD=$dbpass
+
+mysql --host=${dbhost} --port=3306 --database=${dbname} --user=${dbuser}
