9f03a671abe20c8b303e71b49fe6afea516eefc6
Terry Brady <terrywbrady@gmail.com>
2020-10-28 10:25:00 -0700

    init script

--- bin/merritt-docker-dev-init.sh
+++ bin/merritt-docker-dev-init.sh
@@ -4,5 +4,15 @@ cd
 
 rm -rf merritt-docker merritt-docker-prv
 
-git clone git@github.com:cdlib/merritt-docker.git
+git clone git@github.com:CDLUC3/merritt-docker.git
 git clone git@github.com:cdlib/merritt-docker-prv.git
+
+cp -r merritt-docker-prv/* merritt-docker/mrt-services/no-track
+
+cd merritt-docker
+git submodule update --remote --recursive -- .
+
+cd ../merritt-docker/mrt-dependencies
+sudo docker-compose build
+cd ../mrt-services
+sudo docker-compose build
