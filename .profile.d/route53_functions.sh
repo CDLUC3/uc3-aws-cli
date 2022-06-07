09253da942439f6547f78d38306d3d53350230ca
Ashley Gould <agould@ucop.edu>
2022-06-07 15:32:21 -0700

    add route53_functions.sh

---
+++ .profile.d/route53_functions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Route53
+route53-hz-list() {
+    aws route53 list-hosted-zones | jq -r '.HostedZones[].Name'
+}
+
+route53-hz-show-id() {
+    DOMAIN_NAME=${1%%.}.
+    aws route53 list-hosted-zones | \
+	jq -r ".HostedZones[] | select(.Name == \"$DOMAIN_NAME\") | .Id" | \
+	awk -F'/' '{print $3}'
+}
+
+route53-hz-show() {
+    HOSTED_ZONE_ID=$(route53-hz-show-id $1)
+    aws route53 get-hosted-zone --id $HOSTED_ZONE_ID | jq -r
+}
+
+route53-recordset-list() {
+    HOSTED_ZONE_ID=$(route53-hz-show-id $1)
+    aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID | jq -r
+}
