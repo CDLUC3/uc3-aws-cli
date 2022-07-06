d5748df13a604318573d4a18064282057fb72b09
Ashley Gould <agould@ucop.edu>
2022-07-06 15:39:09 -0700

    add more cloud9 funcs

--- .profile.d/cloud9_functions.sh
+++ .profile.d/cloud9_functions.sh
@@ -15,6 +15,24 @@ cloud9-env-show-id() {
     aws cloud9 describe-environments --cli-input-json "$(aws --no-cli-pager cloud9 list-environments)" | jq -r ".environments[] | select(.name == \"$ENV_NAME\") | .id"
 }
 
+cloud9-env-show-memberships() {
+    ENV_NAME=$1
+    aws --no-cli-pager --output yaml cloud9 describe-environment-memberships --environment-id $(cloud9-env-show-id $ENV_NAME)
+}
+
+cloud9-env-add-membership() {
+    ENV_NAME=$1
+    USER_ARN=$2
+    PERMS=$3
+    aws --no-cli-pager --output yaml cloud9 create-environment-membership --environment-id $(cloud9-env-show-id $ENV_NAME) --user-arn $USER_ARN --permissions $PERMS
+}
+
+cloud9-env-remove-membership() {
+    ENV_NAME=$1
+    USER_ARN=$2
+    aws --no-cli-pager --output yaml cloud9 delete-environment-membership --environment-id $(cloud9-env-show-id $ENV_NAME) --user-arn $USER_ARN
+}
+
 cloud9-env-delete() {
     ENV_NAME=$1
     aws cloud9 delete-environment --environment-id $(cloud9-env-show-id $ENV_NAME)
@@ -40,3 +58,16 @@ cloud9-env-delete() {
 # 
 # 
 # aws cloud9 describe-environments --cli-input-json "$(aws --no-cli-pager cloud9 list-environments)" | jq -r ".environments[] | select(.name == \"AshleyFirstPass\") | ."
+
+# agould@localhost:~/.aws> aws --no-cli-pager --output yaml cloud9 describe-environment-memberships --environment-id 141e58ccdbcb478ab34c00e34dafbfd8
+# memberships:
+# - environmentId: 141e58ccdbcb478ab34c00e34dafbfd8
+#   lastAccess: '2022-07-06T09:19:23-07:00'
+#   permissions: owner
+#   userArn: arn:aws:sts::671846987296:assumed-role/AWSReservedSSO_uc3-dev-intern_1b6c0210f99781dd/agould
+#   userId: AROAZY3JTOYQFDTW6EBKL:agould
+# - environmentId: 141e58ccdbcb478ab34c00e34dafbfd8
+#   permissions: read-write
+#   userArn: arn:aws:sts::671846987296:assumed-role/AWSReservedSSO_uc3-dev-intern_1b6c0210f99781dd/ediakone
+#   userId: AROAZY3JTOYQFDTW6EBKL:ediakone
+
