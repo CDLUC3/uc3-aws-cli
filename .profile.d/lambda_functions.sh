361b29b86fd8f943e56ddca8f5cf099adcdb7e16
Ashley Gould <agould@ucop.edu>
2022-07-21 12:57:22 -0700

    update lambda_runcs

--- .profile.d/lambda_functions.sh
+++ .profile.d/lambda_functions.sh
@@ -6,27 +6,28 @@ lambda-function-list() {
     aws lambda list-functions | jq -r .Functions[].FunctionName
 }
 
-lambda-function-policy() {
-    aws lambda get-policy --function-name $1 --output text | jq -r . 2>/dev/null
-}
-
 lambda-function-show() {
-    aws lambda get-function-configuration --function-name $1
-}
-
-lambda-function-full() {
     aws lambda get-function --function-name $1
 }
 
-lambda-update() {
-    bucket=$1
-    key=$2
-    name=$3
-    aws lambda update-function-code --s3-bucket $bucket --s3-key $key --function-name $name
+lambda-function-show-policy() {
+    aws lambda get-policy --function-name $1 --output text | jq -r . 2>/dev/null
+}
+
+lambda-function-show-config() {
+    aws lambda get-function-configuration --function-name $1
 }
 
 
-lambda-delete() {
-    aws lambda delete-function --function-name $1
-}
+#lambda-function-update() {
+#    bucket=$1
+#    key=$2
+#    name=$3
+#    aws lambda update-function-code --s3-bucket $bucket --s3-key $key --function-name $name
+#}
+#
+#
+#lambda-function-delete() {
+#    aws lambda delete-function --function-name $1
+#}
 
