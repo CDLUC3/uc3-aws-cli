7420879d03f843f2b4c72ff8ae8c4e396144edcf
Ashley Gould <agould@ucop.edu>
2022-07-21 14:33:15 -0700

    rename dir .profile.d -> profile.d. more visible to project

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-[ -d ~/.profile.d ] || mkdir -v ~/.profile.d
-cp -v .profile.d/* ~/.profile.d/
-[ -d ~/.aws/cli ] || mkdir -p -v ~/.aws/cli
-cp -v .aws/cli/* ~/.aws/cli/
 [ -d ~/bin ] || mkdir -p -v ~/bin
 cp -v bin/* ~/bin/
+[ -d ~/.profile.d ] || mkdir -v ~/.profile.d
+cp -v profile.d/* ~/.profile.d/
+#[ -d ~/.aws/cli ] || mkdir -p -v ~/.aws/cli
+#cp -v aws_aliases ~/.aws/cli/alias
 
