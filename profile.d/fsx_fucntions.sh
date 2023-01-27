0bacfb7e4fcc1e2e709d3fef960dff090f6d2da0
Ashley Gould <agould@ucop.edu>
2023-01-27 14:04:30 -0800

    updated fsx funcs

--- profile.d/fsx_fucntions.sh
+++ profile.d/fsx_fucntions.sh
@@ -9,7 +9,7 @@ fsx-fs-all() {
         filesystems=$(echo $response | jq -r '.FileSystems[]')
         nextmarker=$(echo $response | jq -r '.NextMarker')
     done
-    echo $filesystems
+    echo $filesystems | jq -r .
 }
 
 fsx-fs-list() {
@@ -18,17 +18,7 @@ fsx-fs-list() {
 
 fsx-fs-show() {
     NAME=$1
-    for fs_id in $(fsx-fs-all | jq -r '.FileSystemId'); do
-	response=$(aws fsx describe-file-systems --file-system-ids $fs_id)
-	fs_name=$(echo $response | jq -r '.FileSystems[].Tags[] | select(.Key == "Name") | .Value')
-	if [ $fs_name == $NAME ]; then
-            if $(which yq 2>&1 > /dev/null); then
-	        echo $response | yq -yr '.'
-            else
-	        echo $response | jq -r
-	    fi
-	fi
-    done
+    fsx-fs-all | jq -r "select(.Tags[] | select(.Key == \"Name\" and .Value == \"$NAME\"))"
 }
 
 
