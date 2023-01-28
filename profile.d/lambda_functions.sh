f39b78fe86d9e6dee2d974d11504732508b3c77c
Ashley Gould <agould@ucop.edu>
2023-01-27 17:02:17 -0800

    lambda funcs ready for v2

--- profile.d/lambda_functions.sh
+++ profile.d/lambda_functions.sh
@@ -3,19 +3,19 @@
 # Lambda query functions
 
 lambda-function-list() {
-    aws lambda list-functions | jq -r .Functions[].FunctionName
+    $AWSBIN lambda list-functions | jq -r .Functions[].FunctionName
 }
 
 lambda-function-show() {
-    aws lambda get-function --function-name $1
+    $AWSBIN lambda get-function --function-name $1 | yq -ry .
 }
 
 lambda-function-show-policy() {
-    aws lambda get-policy --function-name $1 --output text | jq -r . 2>/dev/null
+    $AWSBIN lambda get-policy --function-name $1 --output text | yq -ry . 2>/dev/null
 }
 
 lambda-function-show-config() {
-    aws lambda get-function-configuration --function-name $1
+    $AWSBIN lambda get-function-configuration --function-name $1 | yq -ry . 
 }
 
 
@@ -23,11 +23,11 @@ lambda-function-show-config() {
 #    bucket=$1
 #    key=$2
 #    name=$3
-#    aws lambda update-function-code --s3-bucket $bucket --s3-key $key --function-name $name
+#    $AWSBIN lambda update-function-code --s3-bucket $bucket --s3-key $key --function-name $name
 #}
 #
 #
 #lambda-function-delete() {
-#    aws lambda delete-function --function-name $1
+#    $AWSBIN lambda delete-function --function-name $1
 #}
 
