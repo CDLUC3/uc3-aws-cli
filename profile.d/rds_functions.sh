abd3126d38300f4bc8d927576193ab65fe46ee29
Ashley Gould <agould@ucop.edu>
2023-01-27 17:04:07 -0800

    rds funs ready for v2, but untested

--- profile.d/rds_functions.sh
+++ profile.d/rds_functions.sh
@@ -1,23 +1,23 @@
 rds-db-list() {
-  aws rds  describe-db-instances | jq -r '.DBInstances[].DBInstanceIdentifier'
+  $AWSBIN rds  describe-db-instances | jq -r '.DBInstances[].DBInstanceIdentifier'
 }
 
 rds-db-show() {
   DB_NAME=$1
-  aws rds describe-db-instances --db-instance-identifier $DB_NAME
+  $AWSBIN rds describe-db-instances --db-instance-identifier $DB_NAME | yq -ry .
 }
 
 rds-db-show-arn() {
   DB_NAME=$1
-  aws rds describe-db-instances --db-instance-identifier $DB_NAME | jq -r '.DBInstances[].DBInstanceArn'
+  $AWSBIN rds describe-db-instances --db-instance-identifier $DB_NAME | jq -r '.DBInstances[].DBInstanceArn'
 }
 
 rds-db-show-fqdn() {
   DB_NAME=$1
-  aws rds describe-db-instances --db-instance-identifier $DB_NAME | jq -r '.DBInstances[].Endpoint.Address'
+  $AWSBIN rds describe-db-instances --db-instance-identifier $DB_NAME | jq -r '.DBInstances[].Endpoint.Address'
 }
 
 rds-db-show-tags() {
   DB_NAME=$1
-  aws rds list-tags-for-resource --resource-name $(rds-db-show-arn $DB_NAME)
+  $AWSBIN rds list-tags-for-resource --resource-name $(rds-db-show-arn $DB_NAME) | yq -ry .
 }
