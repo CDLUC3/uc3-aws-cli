65d52da3734d49f6f59d0cbcc0087c55a50788ec
Ashley Gould <agould@ucop.edu>
2024-07-24 15:42:13 -0700

    same

--- profile.d/opensearch_fuctions.sh
+++ profile.d/opensearch_fuctions.sh
@@ -1,10 +1,6 @@
 # Shell fuctions for querying OpenSearch
 #
 
-os-versions-list() {
-    aws opensearch list-versions
-}
-
 os-domain-list() {
     $AWSBIN opensearch list-domain-names | yq -r '.DomainNames[].DomainName'
 }
@@ -30,9 +26,29 @@ os-domain-show-config() {
     $AWSBIN opensearch describe-domain-config --domain-name $NAME
 }
 
+os-domain-health() {
+    NAME=$1
+    $AWSBIN opensearch describe-domain-health --domain-name $NAME
+}
+
+os-domain-nodes() {
+    NAME=$1
+    $AWSBIN opensearch describe-domain-nodes --domain-name $NAME
+}
+
 os-domain-update() {
     NAME=$1
     $AWSBIN opensearch start-service-software-update --domain-name $NAME
+}
+
+os-domain-show-scheduled-actions() {
+    NAME=$1
+    $AWSBIN opensearch list-scheduled-actions --domain-name $NAME
+}
+
+os-domain-show-change-progress() {
+    NAME=$1
+    $AWSBIN opensearch describe-domain-change-progress --domain-name $NAME
 }
 
 os-domain-show-upgrade-versions() {
@@ -41,17 +57,21 @@ os-domain-show-upgrade-versions() {
     $AWSBIN opensearch get-compatible-versions --domain-name $NAME
 }
 
-os-scheduled-actions-list() {
-    NAME=$1
-    $AWSBIN opensearch list-scheduled-actions --domain-name $NAME
-}
-
-# An error occurred (AccessDeniedException) when calling the UpgradeDomain operation: [User: arn:aws:sts::451826914157:assumed-role/uc3-aws-ops/i-097c2bb827b182cf3 is not authorized to perform: es:UpgradeDomain on resource: arn:aws:es:us-west-2:451826914157:domain/os-uc3-logging-stg
 os-domain-upgrade() {
     NAME=$1
     VERSION=$2
     $AWSBIN opensearch upgrade-domain --domain-name $NAME --target-version $VERSION --perform-check-only
 }
+
+
+
+
+# requested permissions:
+#es:GetCompatibleVersions
+#es:UpgradeDomain
+#es:DescribeDomainHealth
+#es:DescribeDomainNodes
+#es:ListScheduledActions
 
 #After testing your changes I find there are more permissions I am missing.  Please add also:
 #```
@@ -83,5 +103,3 @@ os-domain-upgrade() {
 # An error occurred (AccessDeniedException) when calling the ListScheduledActions operation: User: arn:aws:sts::451826914157:assumed-role/uc3-aws-ops/i-097c2bb827b182cf3 is not authorized to perform: es:ListScheduledActions 
 #
 #
-# ApiError
-# User: arn:aws:sts::451826914157:assumed-role/AWSReservedSSO_cdl-reports_0e48e1ad8bce75a9/agould is not authorized to perform: es:ListNotificationsV2 on resource: arn:aws:es:us-east-1:451826914157:domain/* because no identity-based policy allows the es:ListNotificationsV2 action
