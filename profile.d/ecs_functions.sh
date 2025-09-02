a3b503cdbccdc3b75360494b93b01650668dff36
Ashley Gould <agould@ucop.edu>
2025-09-02 15:24:23 -0700

    fix ecs_functions

--- profile.d/ecs_functions.sh
+++ profile.d/ecs_functions.sh
@@ -27,93 +27,100 @@ ecs-service-list() {
   if [ $# -gt 0 ]; then
     CLUSTER_NAME=$1
     $AWSBIN ecs list-services --cluster $CLUSTER_NAME | yq -r '.serviceArns[]' | awk -F '/' '{print $3}'
-  else
+  elif [ $# -eq 0 ]; then
     for CLUSTER_NAME in $(ecs-cluster-list); do
       echo "${CLUSTER_NAME}:"
       $AWSBIN ecs list-services --cluster $CLUSTER_NAME | yq -r '.serviceArns[]' | awk -F '/' '{print "  " $3}'
       echo
     done
+  else
+    echo "Usage: ecs-service-list [cluster name]"
   fi
 }
 
 ecs-service-show-arn() {
-  if [ $# -eq 0 ]; then
-    echo "Usage: ecs-service-show-arn <service name>"
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-service-show-arn <cluster name> <service name>"
   else  
-    SERVICE_NAME=$1
-    for CLUSTER_NAME in $(ecs-cluster-list); do
-      $AWSBIN ecs list-services --cluster $CLUSTER_NAME | yq -r '.serviceArns[]' | grep $SERVICE_NAME
-    done
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    $AWSBIN ecs list-services --cluster $CLUSTER_NAME | yq -r '.serviceArns[]' | grep $SERVICE_NAME
   fi
 }
 
 ecs-service-show() {
-  if [ $# -eq 0 ]; then
-    echo "Usage: ecs-service-show <service name>"
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-service-show <cluster name> <service name>"
   else  
-    SERVICE_NAME=$1
-    SERVICE_ARN=$(ecs-service-show-arn $SERVICE_NAME)
-    CLUSTER_NAME=$(echo $SERVICE_ARN | awk -F '/' '{print $2}')
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
     $AWSBIN ecs describe-services --cluster $CLUSTER_NAME --service $SERVICE_NAME  | yq -ry 'del(.services[].events)'
   fi
 }
 
 ecs-service-show-events() {
-  if [ $# -eq 0 ]; then
-    echo "Usage: ecs-service-show <service name>"
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-service-show-events <cluster name> <service name>"
   else  
-    SERVICE_NAME=$1
-    SERVICE_ARN=$(ecs-service-show-arn $SERVICE_NAME)
-    CLUSTER_NAME=$(echo $SERVICE_ARN | awk -F '/' '{print $2}')
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
     $AWSBIN ecs describe-services --cluster $CLUSTER_NAME --service $SERVICE_NAME  | yq -ry '.services[].events'
   fi
 }
 
-#aws ecs update-service --cluster dmp-tool-dev-ecs-cluster-Fargate --service dmp-tool-dev-ecs-apollo --force-new-deployment
 ecs-service-update() {
-  SERVICE_NAME=$1
-  SERVICE_ARN=$(ecs-service-show-arn $SERVICE_NAME)
-  CLUSTER_NAME=$(echo $SERVICE_ARN | awk -F '/' '{print $2}')
-  $AWSBIN ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --force-new-deployment
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-service-update <cluster name> <service name>"
+  else  
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    $AWSBIN ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --force-new-deployment
+  fi
 }
 
-ecs-taskdef-for-service-show-arn() {
-  if [ $# -eq 0 ]; then
-    echo "Usage: ecs-service-show <service name>"
+ecs-taskdef-show-arn() {
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-taskdef-show-arn <cluster name> <service name>"
   else  
-    SERVICE_NAME=$1
-    SERVICE_ARN=$(ecs-service-show-arn $SERVICE_NAME)
-    CLUSTER_NAME=$(echo $SERVICE_ARN | awk -F '/' '{print $2}')
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
     $AWSBIN ecs describe-services --cluster $CLUSTER_NAME --service $SERVICE_NAME  | yq -r '.services[].deployments[].taskDefinition'
   fi
 }
 
-ecs-taskdef-for-service-show() {
-  SERVICE_NAME=$1
-  $AWSBIN ecs describe-task-definition --task-definition $(ecs-taskdef-for-service-show-arn $SERVICE_NAME)
+ecs-taskdef-show() {
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-taskdef-show <cluster name> <service name>"
+  else  
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    $AWSBIN ecs describe-task-definition --task-definition $(ecs-taskdef-for-service-show-arn $CLUSTER_NAME $SERVICE_NAME)
+  fi
 }
 
-# agould@localhost:~/git/github/cdluc3/uc3-aws-cli/profile.d> aws ecs list-tasks --cluster dmp-tool-dev-ecs-cluster-Fargate --service-name dmp-tool-dev-ecs-apollo
-ecs-task-list-for-service() {
-  SERVICE_NAME=$1
-  SERVICE_ARN=$(ecs-service-show-arn $SERVICE_NAME)
-  CLUSTER_NAME=$(echo $SERVICE_ARN | awk -F '/' '{print $2}')
-  $AWSBIN ecs list-tasks --cluster $CLUSTER_NAME --service-name $SERVICE_NAME | yq -r '.taskArns[]'
-}
-
-ecs-task-list-for-cluster() {
-  CLUSTER_NAME=$1
-  $AWSBIN ecs list-tasks --cluster $CLUSTER_NAME | yq -r '.taskArns[]'
+ecs-task-list() {
+  if [ $# -eq 2 ]; then
+    CLUSTER_NAME=$1
+    SERVICE_NAME=$2
+    $AWSBIN ecs list-tasks --cluster $CLUSTER_NAME --service-name $SERVICE_NAME | yq -r '.taskArns[]'
+  elif [ $# -eq 1 ]; then
+    CLUSTER_NAME=$1
+    $AWSBIN ecs list-tasks --cluster $CLUSTER_NAME | yq -r '.taskArns[]'
+  else
+    echo "Usage: ecs-task-list <cluster name> <service name>"
+  fi
 }
 
 # agould@localhost:~/git/github/cdluc3/uc3-aws-cli/profile.d> aws ecs describe-tasks --cluster dmp-tool-dev-ecs-cluster-Fargate --tasks arn:aws:ecs:us-west-2:671846987296:task/dmp-tool-dev-ecs-cluster-Fargate/dc57b0dc02f2465087757bf002213f1d| json2yaml.py |less
 ecs-task-show() {
-  #if [ $# -nq 2 ]; then
-  if [ "$#" != 2 ]; then
-    echo "Usage: ecs-task-show <cluster name> <task arn>"
+  if [ $# -ne 2 ]; then
+    echo "Usage: ecs-taskdef-show <cluster name> <service name>"
+    #echo "Usage: ecs-task-show <cluster name> <task arn>"
   else  
     CLUSTER_NAME=$1
-    TASK_ARN=$2
+    SERVICE_NAME=$2
+    TASK_ARN=$(ecs-task-list $CLUSTER_NAME $SERVICE_NAME)
     $AWSBIN ecs describe-tasks --cluster $CLUSTER_NAME --tasks $TASK_ARN
   fi
 }
+
