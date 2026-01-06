44d64a7d93119ae13dfdb9710f78f2c1dcb2fea7
Ashley Gould <agould@ucop.edu>
2026-01-06 15:16:17 -0800

    add some eds funcs for snapshots

--- profile.d/rds_functions.sh
+++ profile.d/rds_functions.sh
@@ -21,3 +21,23 @@ rds-db-show-tags() {
   DB_NAME=$1
   $AWSBIN rds list-tags-for-resource --resource-name $(rds-db-show-arn $DB_NAME) | yq -ry .
 }
+
+rds-db-snapshot-list() {
+  $AWSBIN rds describe-db-snapshots | yq -r '.DBSnapshots[].DBSnapshotIdentifier'
+}
+
+rds-db-snapshot-list-manual() {
+  $AWSBIN rds describe-db-snapshots --snapshot-type manual | yq -r '.DBSnapshots[].DBSnapshotIdentifier'
+}
+
+rds-db-snapshot-list-for-instance() {
+  DB_NAME=$1
+  $AWSBIN rds describe-db-snapshots --db-instance-identifier $DB_NAME | yq -r '.DBSnapshots[].DBSnapshotIdentifier'
+}
+
+rds-db-snapshot-show() {
+  DB_SNAPSHOT_ID=$1
+  $AWSBIN rds describe-db-snapshots --db-snapshot-identifier $DB_SNAPSHOT_ID | yq -ry .
+}
+
+
