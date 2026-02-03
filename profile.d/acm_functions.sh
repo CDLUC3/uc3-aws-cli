2f724ed941fb1da54aeee92993ee6e8ba8496794
Ashley Gould <agould@ucop.edu>
2026-02-03 09:14:13 -0800

    add func acm-route53-domainvalidation-list

--- profile.d/acm_functions.sh
+++ profile.d/acm_functions.sh
@@ -23,3 +23,12 @@ acm-cert-delete() {
     $AWSBIN acm delete-certificate --certificate-arn $CERTARN
 }
 
+
+acm-route53-domainvalidation-list() {
+    CERTARNS=$($AWSBIN  acm list-certificates | yq -r ".CertificateSummaryList[].CertificateArn")
+    for certarn in $CERTARNS; do
+	echo $certarn
+        $AWSBIN acm describe-certificate --certificate-arn $certarn | \
+	    yq -ry '.Certificate.DomainValidationOptions[]'
+    done
+}
